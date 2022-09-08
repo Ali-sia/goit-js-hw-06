8674efb8a71139b84c96412d92893d311fb29a9a
aliesia-zahorui <aliesia@ukr.net>
2022-09-08 16:30:24 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientList = document.querySelector('#ingredients');
+const liRef = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  liRef[i] = document.createElement('li');
+  liRef[i].textContent = ingredients[i];
+  ingredientList.appendChild(liRef[i]);
+}
