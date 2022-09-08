78f6e69643e0d8725caf96758877bde60c5ac945
aliesia-zahorui <aliesia@ukr.net>
2022-09-08 12:39:54 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+const categoriesEl = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoriesEl.length}`);
+
+const categoriesArray = [...categoriesEl];
+const categoryName = categoriesArray.map(categories => categories.children[0].textContent);
+const subcategoryLength = categoriesArray.map(categories => categories.children[1].children.length);
+
+for (let i = 0; i < categoriesArray.length; i++) {
+  console.log(`Category: ${categoryName[i]}\nElements: ${subcategoryLength[i]}`);
+}
+
+// const categoryArray = [...categoriesEl]
+//   .map(
+//     categories =>
+//       `Category: ${categories.children[0].textContent}\nElements: ${categories.children[1].children.length}`
+//   )
+//   .join('\n');
+// console.log(categoryArray);
