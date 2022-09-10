9bbe6925bf091625b7e39d0ba0e38883cdcde3f2
aliesia-zahorui <aliesia@ukr.net>
2022-09-10 15:01:19 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxHolder: document.querySelector('#boxes'),
+};
+
+console.log('🚀 -> createBoxes -> boxAmount', refs.boxAmount);
+
+refs.createBtn.addEventListener('click', createBoxes);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(event) {
+  const boxAmount = refs.input.value;
+  const startBoxSize = 30;
+  let size;
+
+  for (let i = 1; i <= boxAmount; i += 1) {
+    console.log('🚀 -> createBoxes -> i = ', i);
+    size = startBoxSize + i * 10;
+
+    refs.boxHolder.insertAdjacentHTML(
+      'beforeend',
+      `<div style = "width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`
+    );
+  }
+}
+
+function destroyBoxes() {
+  refs.boxHolder.innerHTML = '';
+}
