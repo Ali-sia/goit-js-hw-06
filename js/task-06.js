7255afb2d8dad6712eef1e1b632897bcd6a9a901
aliesia-zahorui <aliesia@ukr.net>
2022-09-10 20:57:53 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const inputRef = document.querySelector('#validation-input');
-const validateLength = inputRef.dataset.length;
+const validateLength = Number(inputRef.dataset.length);
 
 const validate = event => {
-  if (event.currentTarget.value.length > validateLength) {
-    inputRef.classList.remove('valid');
-    inputRef.classList.add('invalid');
-  } else {
+  if (event.currentTarget.value.length === validateLength) {
     inputRef.classList.remove('invalid');
     inputRef.classList.add('valid');
+  } else {
+    inputRef.classList.remove('valid');
+    inputRef.classList.add('invalid');
   }
 };
 
